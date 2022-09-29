0c758c5c11e4f0246320455fa114fb975b57108d
DiogoG8 <110616195+DiogoG8@users.noreply.github.com>
2022-09-29 14:47:17 +0100

    Test Chalk

---
+++ name.js
@@ -0,0 +1,11 @@
+const name="Iryna";
+const name2="João";
+const name3="Elif";
+const name4="Isamu";
+
+import chalk from 'chalk';
+console.log(chalk.blue(name));
+console.log(chalk.red(name2));
+console.log(chalk.green(name3));
+console.log(chalk.yellow(name4));
+
